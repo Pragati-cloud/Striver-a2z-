91d90e9e02210e6ef1cccb74595a1d618f861b62
Pragati Mishra <144036589+Pragati-cloud@users.noreply.github.com>
2025-07-31 23:57:44 +0530

    Create SecondLarge.java

---
+++ DAY6/SecondLarge.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SecondLargest {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int[] arr = new int[6];
+    for(int i =0; i<6;i++){
+      arr[i]= sc.nextInt();
+    }
+    System.out.println(Arrays.toString(arr));
+    Largeelem1(arr);
+    Largeelem2(arr);
+
+  }
+
+  public static void Largeelem1(int[] arr){
+    // Brute force 
+    Arrays.sort(arr);
+    int n =arr.length;
+    System.out.println(arr[n-2]); //if does not contain duplicate
+  }
+  public static void Largeelem2(int[] arr){
+    //optimal
+
+    int max = arr[0];
+    int secondlarge =-1;
+    int n =arr.length;
+    for(int i =1; i<n ;i++){
+        if(max <arr[i]) max=arr[i];
+    }
+    
+    for(int i=0;i<n;i++){
+      if(arr[i]> secondlarge && arr[i]!=max){
+        secondlarge=arr[i];
+      }
+    }
+    System.out.println(secondlarge);
+
+  }
+}
