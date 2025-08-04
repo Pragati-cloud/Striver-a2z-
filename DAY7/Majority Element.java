b0125884fd44ff795412318f95d55d7c62013362
Pragati Mishra <144036589+Pragati-cloud@users.noreply.github.com>
2025-08-04 23:38:43 +0530

    Create Majority Element.java

---
+++ DAY7/Majority Element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        int n = nums.length;
+         for (int num : nums) {
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
+            if (mp.get(num) > n / 2) {
+                return num; 
+            }
+        }
+
+        return -1;
+    }
+}
